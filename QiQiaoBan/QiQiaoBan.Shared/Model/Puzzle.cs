f0a74ddbd3964074ffe5cae2a9364d20c5b51451
makowildcat <mako_wildcat@hotmail.com>
2015-02-14 00:03:08 +0100

    Add model Puzzle

---
+++ QiQiaoBan/QiQiaoBan.Shared/Model/Puzzle.cs
@@ -0,0 +1,28 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace QiQiaoBan.Model
+{
+    public class Puzzle : ObservableObject
+    {
+        public const string NamePropertyName = "Name";
+        private string _name;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                Set(NamePropertyName, ref _name, value);
+            }
+        }
+
+        public const string PiecesPropertyName = "Pieces";
+        public ObservableCollection<Piece> Pieces;        
+    }
+}
