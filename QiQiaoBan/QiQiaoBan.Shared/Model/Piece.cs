463e0da380ee44d90d79d098c5465b576e10e3f9
makowildcat <mako_wildcat@hotmail.com>
2015-02-14 00:02:55 +0100

    Add Piece model

---
+++ QiQiaoBan/QiQiaoBan.Shared/Model/Piece.cs
@@ -0,0 +1,54 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QiQiaoBan.Model
+{
+    class Piece : ObservableObject
+    {
+        public const string LeftPropertyName = "Left";
+        private double _left;
+        public double Left
+        {
+            get
+            {
+                return _left;
+            }
+            set
+            {
+                Set(LeftPropertyName, ref _left, value);
+            }            
+        }
+
+        public const string TopPropertyName = "Top";
+        private double _top;
+        public double Top
+        {
+            get
+            {
+                return _top;
+            }
+            set
+            {
+                Set(TopPropertyName, ref _top, value);
+            }
+        }
+
+        public const string AnglePropertyName = "Angle";
+        private double _angle;
+        public double Angle
+        {
+            get
+            {
+                return _angle;
+            }
+            set
+            {
+                Set(AnglePropertyName, ref _angle, value);
+            }
+        }
+
+        public Piece() { }
+    }
+}
