348cd6a90a39051a2c2be83d80bd56e52c87b190
makowildcat <mako_wildcat@hotmail.com>
2015-02-13 23:35:17 +0100

    Add ViewModelLocator

---
+++ QiQiaoBan/QiQiaoBan.Shared/ViewModel/ViewModelLocator.cs
@@ -0,0 +1,26 @@
+﻿using GalaSoft.MvvmLight.Ioc;
+using Microsoft.Practices.ServiceLocation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QiQiaoBan.ViewModel
+{
+    class ViewModelLocator
+    {
+        static ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+
+            SimpleIoc.Default.Register<MainViewModel>();
+        }
+
+        public MainViewModel Main
+        {
+            get
+            {
+                return ServiceLocator.Current.GetInstance<MainViewModel>();
+            }
+        }
+    }
+}
