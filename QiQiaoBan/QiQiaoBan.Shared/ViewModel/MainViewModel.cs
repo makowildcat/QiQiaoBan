d582f18843be29ac2d1c07e88fe91b4cc8c69cc2
makowildcat <mako_wildcat@hotmail.com>
2015-02-13 23:20:34 +0100

    Add class MainViewModel

---
+++ QiQiaoBan/QiQiaoBan.Shared/ViewModel/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QiQiaoBan.ViewModel
+{
+    class MainViewModel : ViewModelBase
+    {
+        private string _title;
+        public string Title {
+            get
+            {
+                return _title;
+            }
+            private set
+            {
+                _title = value;
+            }
+        }
+
+        public MainViewModel()
+        {
+            Title = "QiQiaoBan";
+        }
+    }
+}
