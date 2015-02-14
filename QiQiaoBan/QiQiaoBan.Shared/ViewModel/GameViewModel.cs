29279b45e609e8eeb8bc14cd987083c150cd19e3
makowildcat <mako_wildcat@hotmail.com>
2015-02-14 18:49:33 +0100

    Add PolygonManipulationDeltaCommand

--- QiQiaoBan/QiQiaoBan.Shared/ViewModel/GameViewModel.cs
+++ QiQiaoBan/QiQiaoBan.Shared/ViewModel/GameViewModel.cs
@@ -1,10 +1,12 @@
 ﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 using QiQiaoBan.Model;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Text;
+using Windows.UI.Xaml.Input;
 
 namespace QiQiaoBan.ViewModel
 {
@@ -19,10 +21,8 @@ namespace QiQiaoBan.ViewModel
         public GameViewModel(Puzzle model)
         {
             Model = model;
-            foreach (var piece in Model.Pieces)
-            {
-                Debug.WriteLine(piece.Left + ":" + piece.Top);
-            }
+            Pieces = model.Pieces;
+            this.PolygonManipulationDeltaCommand = new RelayCommand<ManipulationDeltaRoutedEventArgs>(this.ExecuteCommand);
         }
 
         public string Name
@@ -33,12 +33,30 @@ namespace QiQiaoBan.ViewModel
             }
         }
 
+        public const string PiecesPropertyName = "Pieces";
+        private List<Piece> _pieces;
         public List<Piece> Pieces
         {
             get
             {
-                return Model.Pieces;
+                return _pieces;
             }
+            set
+            {
+                Set(PiecesPropertyName, ref _pieces, value);
+            }
+        }
+
+        public RelayCommand<ManipulationDeltaRoutedEventArgs> PolygonManipulationDeltaCommand { get; private set; }    
+        
+        public void ExecuteCommand(ManipulationDeltaRoutedEventArgs parameter)
+        {
+            Windows.UI.Xaml.Shapes.Rectangle rect = parameter.OriginalSource as Windows.UI.Xaml.Shapes.Rectangle;
+            
+            //Pieces[int.Parse(rect.Tag.ToString())].Left += parameter.Delta.Translation.X;
+            //Pieces[int.Parse(rect.Tag.ToString())].Top += parameter.Delta.Translation.Y;
+            Pieces[0].Left += parameter.Delta.Translation.X;
+            Pieces[0].Top += parameter.Delta.Translation.Y;
         }
     }
 }
