4597ae983e11688dda37fa5778d4279fec9209af
makowildcat <mako_wildcat@hotmail.com>
2015-02-14 18:35:32 +0100

    Add ItemsControlCanvas

---
+++ QiQiaoBan/QiQiaoBan.Shared/Common/ItemsControlCanvas.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+
+namespace QiQiaoBan.Common
+{
+    class ItemsControlCanvas : ItemsControl
+    {
+        protected override void PrepareContainerForItemOverride(Windows.UI.Xaml.DependencyObject element, object item)
+        {
+            Binding leftBinding = new Binding() { Path = new PropertyPath("Left"), Mode = BindingMode.TwoWay };
+            Binding topBinding = new Binding() { Path = new PropertyPath("Top"), Mode = BindingMode.TwoWay };
+
+            FrameworkElement contentControl = (FrameworkElement)element;
+            contentControl.SetBinding(Canvas.LeftProperty, leftBinding);
+            contentControl.SetBinding(Canvas.TopProperty, topBinding);
+
+            base.PrepareContainerForItemOverride(element, item);
+        }
+    }
+}
