14eb97a55ea271d0b88491e870af32459455c52a
makowildcat <mako_wildcat@hotmail.com>
2015-02-23 16:36:27 +0100

    Add IntToStringTimeConverter

---
+++ QiQiaoBan/QiQiaoBan.Shared/Helpers/IntToStringTimeConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Xaml.Data;
+
+namespace QiQiaoBan.Helpers
+{
+    class IntToStringTimeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            int n = (int)value;
+            return (n / 60).ToString("00") + ":" + (n % 60).ToString("00");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
